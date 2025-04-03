2461bb397b8ff521ad01380603357f8d901c77e5
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2025-04-04 03:17:03 +0600

    200

--- java/src/main/kotlin/Solutions.kt
+++ java/src/main/kotlin/Solutions.kt
@@ -43,3 +43,35 @@ private fun isSymmetric(left: TreeNode?, right: TreeNode?): Boolean {
             && isSymmetric(n1, n4)
             && isSymmetric(n2, n3)
 }
+
+// https://leetcode.com/problems/number-of-islands/
+fun numIslands(grid: Array<CharArray>): Int {
+    var count = 0
+    for (i in 0..grid.lastIndex) {
+        for (j in 0..grid[i].lastIndex) {
+            if (grid[i][j] == '1') {
+                count++
+                handleIsland(i, j, grid)
+            }
+        }
+    }
+    return count
+}
+
+fun handleIsland(i: Int, j: Int, grid: Array<CharArray>) {
+    if (grid[i][j] == '1') {
+        grid[i][j] = '0'
+        if (i < grid.size - 1) {
+            handleIsland(i + 1, j, grid)
+        }
+        if (j < grid[i].size - 1) {
+            handleIsland(i, j + 1, grid)
+        }
+        if (i > 0) {
+            handleIsland(i - 1, j, grid)
+        }
+        if (j > 0) {
+            handleIsland(i, j - 1, grid)
+        }
+    }
+}
--- java/src/test/kotlin/SolutionsKtTest.kt
+++ java/src/test/kotlin/SolutionsKtTest.kt
@@ -43,4 +43,26 @@ class SolutionsKtTest {
             )
         )
     }
+
+    @Test
+    fun `test numIslands`() {
+        assertEquals(
+            1,
+            numIslands(arrayOf(
+                charArrayOf('1','1','1','1','0'),
+                charArrayOf('1','1','0','1','0'),
+                charArrayOf('1','1','0','0','0'),
+                charArrayOf('0','0','0','0','0')
+            ))
+        )
+        assertEquals(
+            3,
+            numIslands(arrayOf(
+                charArrayOf('1','1','0','0','0'),
+                charArrayOf('1','1','0','0','0'),
+                charArrayOf('0','0','1','0','0'),
+                charArrayOf('0','0','0','1','1')
+            ))
+        )
+    }
 }
