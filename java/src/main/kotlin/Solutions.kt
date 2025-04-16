b0a3f5fd37afd4dd4930ffc04125455e5db03f5a
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2025-04-17 01:25:49 +0600

    Fix 1768

--- java/src/main/kotlin/Solutions.kt
+++ java/src/main/kotlin/Solutions.kt
@@ -293,7 +293,7 @@ fun mergeAlternately(word1: String, word2: String): String {
     val w1 = word1.toCharArray()
     val w2 = word2.toCharArray()
     return buildString {
-        for (i in 0..(maxOf(w1.size, w2.size) - 1)) {
+        for (i in 0..<maxOf(w1.size, w2.size)) {
             if (i < w1.size) append(w1[i])
             if (i < w2.size) append(w2[i])
         }
