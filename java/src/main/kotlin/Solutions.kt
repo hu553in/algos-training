b96cc7a85a50500689581d046636eb8431d50c6a
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2025-04-17 00:18:25 +0600

    Add 1768

--- java/src/main/kotlin/Solutions.kt
+++ java/src/main/kotlin/Solutions.kt
@@ -287,3 +287,15 @@ fun calculateTop(statsPerDay: List<List<UserPerDay>>): Top {
         max
     )
 }
+
+// https://leetcode.com/problems/merge-strings-alternately/
+fun mergeAlternately(word1: String, word2: String): String {
+    val w1 = word1.toCharArray()
+    val w2 = word2.toCharArray()
+    return buildString {
+        for (i in 0..(maxOf(w1.size, w2.size) - 1)) {
+            if (i < w1.size) append(w1[i])
+            if (i < w2.size) append(w2[i])
+        }
+    }
+}
